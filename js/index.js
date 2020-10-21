80a6f223cd7354c19404c460f51f1f71fa147f63
s96116157 <s96116157@gmail.com>
2020-10-22 01:50:53 +0800

    202010220150

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-function get_json() {
-    $.getJSON('js/json/info.json', function (data) {
-        $.each(data.info, function (i, emp) {
-            console.log(emp.card);
-        });
-    }).error(function () {
-        console.log('error');
-    });
-};
+var today_card = ["", "", "", ""];
+$.getJSON('https://s96116157.github.io/js/json/info.json', function (data) {
+    for (i = 0; i < 4; i++) {
+        today_card[i] = data.info[i].card;
+    }
+}).error(function () {
+    console.log('error');
+});
 
 var x = '';
 var vm = new Vue({
@@ -18,7 +17,8 @@ var vm = new Vue({
     },
     methods: {
         prompt() {
-            this.msg = '請選擇下方任一張牌';
+            this.msg = '請選擇下方任一張牌';            
+            this.card_msg = 'OOOOOOO';
             this.show_img()
         },
         show_img() {
@@ -26,25 +26,25 @@ var vm = new Vue({
             Vue.component('v-img', {
                 template: x
             });
+        },
+        change_info() {
         }
     }
 });
 
 function get_img() {
-    get_json();
-    for (i = 0; i < 4; i++) { }
     x = '<div>' +
         '<div class="row">' +
         '<div class="col-md-3"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/A_000.png" class="mx-auto d-block" style="width: 80%;"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/A_001.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/' + today_card[0] + '.png" class="mx-auto d-block" style="width: 80%;" data-toggle="modal" data-target="#exampleModalCenter" type="button"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/' + today_card[1] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
         '<div class="col-md-3"></div>' +
         '</div>' +
         '<h1></h1>' +
         '<div class="row">' +
         '<div class="col-md-3"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/A_002.png" class="mx-auto d-block" style="width: 80%;"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/A_003.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/' + today_card[2] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/' + today_card[3] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
         '<div class="col-md-3"></div>' +
         '</div>' +
         '</div>';
