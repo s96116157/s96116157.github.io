c84e0439e67bcba4aeca7c034da35b46f83fc2ae
s96116157 <s96116157@gmail.com>
2020-10-21 22:40:24 +0800

    202010212240

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var x = '';
+
 var vm = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
@@ -8,8 +10,28 @@ var vm = new Vue({
     methods: {
         prompt() {
             this.msg = '請選擇下方任一張牌';
-            this.isShow = true;
+            this.show_img()
+        },
+        show_img() {
+            get_img();
+            Vue.component('v-img', {
+                template: x
+            });
         }
-
     }
 });
+
+function get_img() {
+    for (i=0;i<4;i++){}
+    x = '<div>'+
+    '<div class="row">'+
+    '<div class="col-6"><img src="image/A_000.png" class="mx-auto d-block" style="width: 80%;"></div>'+
+    '<div class="col-6"><img src="image/A_001.png" class="mx-auto d-block" style="width: 80%;"></div>'+
+    '</div>'+
+    '<h1></h1>'+
+    '<div class="row">'+
+    '<div class="col-6"><img src="image/A_002.png" class="mx-auto d-block" style="width: 80%;"></div>'+
+    '<div class="col-6"><img src="image/A_003.png" class="mx-auto d-block" style="width: 80%;"></div>'+
+    '</div>'+
+    '</div>';
+}
