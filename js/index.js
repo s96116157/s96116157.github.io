6c3dbd0e5c0ff430ff249ab452b90af49f786249
s96116157 <s96116157@gmail.com>
2020-10-22 00:35:28 +0800

    202010220035

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-var x = '';
+function get_json() {
+    $.getJSON('js/json/info.json', function (data) {
+        $.each(data.info, function (i, emp) {
+        });
+    }).error(function () {
+        console.log('error');
+    });
+};
 
+var x = '';
 var vm = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
@@ -22,16 +30,21 @@ var vm = new Vue({
 });
 
 function get_img() {
-    for (i=0;i<4;i++){}
-    x = '<div>'+
-    '<div class="row">'+
-    '<div class="col-6"><img src="image/A_000.png" class="mx-auto d-block" style="width: 80%;"></div>'+
-    '<div class="col-6"><img src="image/A_001.png" class="mx-auto d-block" style="width: 80%;"></div>'+
-    '</div>'+
-    '<h1></h1>'+
-    '<div class="row">'+
-    '<div class="col-6"><img src="image/A_002.png" class="mx-auto d-block" style="width: 80%;"></div>'+
-    '<div class="col-6"><img src="image/A_003.png" class="mx-auto d-block" style="width: 80%;"></div>'+
-    '</div>'+
-    '</div>';
+    get_json();
+    for (i = 0; i < 4; i++) { }
+    x = '<div>' +
+        '<div class="row">' +
+        '<div class="col-md-3"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/A_000.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/A_001.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-md-3"></div>' +
+        '</div>' +
+        '<h1></h1>' +
+        '<div class="row">' +
+        '<div class="col-md-3"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/A_002.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-6 col-md-3"><img src="image/A_003.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div class="col-md-3"></div>' +
+        '</div>' +
+        '</div>';
 }
