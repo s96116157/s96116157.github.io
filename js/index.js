94c69b3700e7034c70d937328aec16fce0b892b1
s96116157 <s96116157@gmail.com>
2020-10-22 10:51:50 +0800

    202010221051

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 var today_card = ["", "", "", ""];
+var today_txt = ["", "", "", ""];
 $.getJSON('https://s96116157.github.io/js/json/info.json', function (data) {
     for (i = 0; i < 4; i++) {
         today_card[i] = data.info[i].card;
+        today_txt[i] = data.info[i].txt;
     }
 }).error(function () {
     console.log('error');
@@ -13,40 +15,56 @@ var vm = new Vue({
     delimiters: ['${', '}'],
     data: {
         msg: '顯示十月運勢',
-        isShow: false
+        card_msg: '',
+        isShow: false,
+        src: ''
     },
     methods: {
         prompt() {
-            this.msg = '請選擇下方任一張牌';            
-            this.card_msg = 'OOOOOOO';
+            this.msg = '請選擇下方任一張牌';
             this.show_img()
         },
         show_img() {
             get_img();
             Vue.component('v-img', {
-                template: x
+                data: function () {
+                    return {
+                        count: 0
+                    };
+                },
+                // 元件的 HTML
+                template: x,
+                methods: {
+                    change_info: function (event) {
+                        targetId = event.currentTarget.id;
+                        var num = targetId.charAt(targetId.length-1);
+                        vm.card_msg = today_txt[num];
+                        vm.src = 'image/' + targetId + '.png'
+                    }
+                }
             });
-        },
-        change_info() {
-            this.msg = 'onclick';   
         }
     }
 });
 
 function get_img() {
+    btn_txt = '" class="col-6 col-md-3 code-btn" type="button" data-toggle="modal" data-target="#show_card_info" v-on:click="change_info($event)">';
     x = '<div>' +
         '<div class="row">' +
         '<div class="col-md-3"></div>' +
-        '<div class="col-6 col-md-3 code-btn" type="button" data-toggle="modal" data-target="#show_card_info">'+
+        '<div id="' + today_card[0] + btn_txt +
         '<img src="image/' + today_card[0] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/' + today_card[1] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div id="' + today_card[1] + btn_txt +
+        '<img src="image/' + today_card[1] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
         '<div class="col-md-3"></div>' +
         '</div>' +
         '<h1></h1>' +
         '<div class="row">' +
         '<div class="col-md-3"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/' + today_card[2] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
-        '<div class="col-6 col-md-3"><img src="image/' + today_card[3] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div id="' + today_card[2] + btn_txt +
+        '<img src="image/' + today_card[2] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
+        '<div id="' + today_card[3] + btn_txt +
+        '<img src="image/' + today_card[3] + '.png" class="mx-auto d-block" style="width: 80%;"></div>' +
         '<div class="col-md-3"></div>' +
         '</div>' +
         '</div>';
