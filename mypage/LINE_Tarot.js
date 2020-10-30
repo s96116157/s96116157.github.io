49bf14a079709910f8c8b032016e69fab6d3585c
s96116157 <s96116157@gmail.com>
2020-10-30 22:16:15 +0800

    pi

--- mypage/LINE_Tarot.js
+++ mypage/LINE_Tarot.js
@@ -3,15 +3,6 @@ var aUrl = "https://s96116157.github.io/js/json/LINE_BOT.json";
 var response = UrlFetchApp.fetch(aUrl); // get feed
 var json_txt = JSON.parse(response.getContentText()); //
 
-//=======================================================================
-var spreadSheetID = "1X5Mtln-MYBhyBRn0RveNOXCkb32A4VTzht1AIGkNvdU";
-var spreadSheet = SpreadsheetApp.openById(spreadSheetID);
-var sheet = spreadSheet.getActiveSheet();
-var lastRow = sheet.getLastRow();
-var lastColumn = sheet.getLastColumn();
-var sheetData = sheet.getSheetValues(1, 1, lastRow, lastColumn);
-//=======================================================================
-
 var txt_3 = {};
 
 function doPost(e) {
@@ -19,17 +10,17 @@ function doPost(e) {
     var clientID = msg.events[0].source.userId;
     var return_txt = '';
     var userMessage = '';
-    console.log('============ msg.events[0] ============');
+    console.log('============ 202010302109 ============');
     console.log(msg.events[0]);
     var replyToken = msg.events[0].replyToken;
     if (typeof replyToken === 'undefined') {
         return;
     }
     var userType = msg.events[0].type;
-    //getUserAnswer(clientID, '');
     switch (userType) {
         case 'follow':
-            return_txt = getJson();
+            return_txt = getJson(0);
+            getUserAnswer(clientID, 99);
             sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
             return;
         case 'unfollow':
@@ -38,7 +29,6 @@ function doPost(e) {
             userMessage = msg.events[0].message.text;
             if (userMessage.indexOf('塔羅') != -1) {
                 return_txt = json_txt[3];
-                //return_txt = txt_3;
                 sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
             }
             else {
@@ -55,27 +45,15 @@ function doPost(e) {
                     "originalContentUrl": "https://s96116157.github.io/image/A_00" + num + ".png",
                     "previewImageUrl": "https://s96116157.github.io/image/A_00" + num + ".png"
                 };
-
-                //response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/info.json'); // get feed
-                response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/LINE_bubble.json');
-                var info_txt = JSON.parse(response.getContentText()); //
-                //var txt_2 = { "type": "text", "text": info_txt['info'][0]['txt'] };
-                //console.log('============== info_txt =================');
-                //console.log(info_txt);
-
-                //sendReplyMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt, txt_2)
-                sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, info_txt[0]);
+                return_txt = getJson(1);
+                getUserAnswer(clientID, 7);
+                sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
             }
             return;
     }
-
     console.log('============== END =================');
-    //==========================================================================================
-    //sendReplyMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
-    //==========================================================================================
 }
 
-//傳送訊息給使用者
 function sendReplyMessage(CHANNEL_ACCESS_TOKEN, replyToken, txt_1, txt_2) {
     if (txt_2 == '') {
         var msg = [txt_1];
@@ -111,43 +89,41 @@ function sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, msg) {
     });
 }
 
-//判斷使用者回答到第幾題
 function getUserAnswer(clientID, clientMessage) {
-    /*
-        var returnData = [];
-        for (var i = lastRow - 1; i >= 0; i--) {
-            if (sheetData[i][0] == clientID && sheetData[i][lastColumn - 1] == "") {
-                for (var j = 1; j <= lastColumn - 1; j++) {
-                    if (sheetData[i][j] == "") { break; }
-                }
-                sheet.getRange(i + 1, j + 1).setValue(clientMessage);
-                //如果使用者已經回答了最後一題，就把完成時間填上。不然就送出下一題給使用者
-                if (j + 2 == lastColumn) {
-                    sheet.getRange(i + 1, lastColumn).setValue(Date());
-                    returnData = [i + 1, 0];
-                }
-                else {
-                    returnData = [i + 1, j + 2];
-                }
-                return returnData;
-                break;
-            }
+    //=======================================================================
+    var spreadSheetID = "1X5Mtln-MYBhyBRn0RveNOXCkb32A4VTzht1AIGkNvdU";
+    var spreadSheet = SpreadsheetApp.openById(spreadSheetID);
+    var sheet = spreadSheet.getActiveSheet();
+    var lastRow = sheet.getLastRow();
+    var lastColumn = sheet.getLastColumn();
+    var sheetData = sheet.getSheetValues(1, 1, lastRow, lastColumn);
+    //=======================================================================
+    var time = Utilities.formatDate(new Date(), "GMT+8", "yyyy-MM-dd HH:mm:ss");
+    for (var i = lastRow - 1; i >= 0; i--) {
+        if (sheetData[i][0] == clientID) {
+            sheet.getRange(i + 1, 2).setValue(clientMessage);
+            sheet.getRange(i + 1, 3).setValue(time);
+            return;
         }
-    */
-    //如果使用者還沒有回答過任何資料，就新增加一列在最後，把使用者ID輸入並開始送出題目
+    }
+
     sheet.insertRowAfter(lastRow);
     sheet.getRange(lastRow + 1, 1).setValue(clientID);
-    sheet.getRange(lastRow + 1, 2).setValue(0);
-    //returnData = [lastRow + 1, 2];
-    //console.log('=================== returnData ==================');
-    //console.log(returnData);
-    //return returnData;
+    sheet.getRange(lastRow + 1, 2).setValue(clientMessage);
+    sheet.getRange(lastRow + 1, 3).setValue(time);
+    return;
 }
 
-function getJson() {
+function getJson(i) {
     var response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/LINE_bubble.json');
     var info_txt = JSON.parse(response.getContentText()); //
-    return info_txt[0];
+    if (i == 1) {
+        for (var num = 0; num < 0; num++) {
+            info_txt[1]['contents']['contents'][2]['hero']['url'] = 'https://s96116157.github.io/image/A_001.png';
+        }
+    }
+    //console.log(info_txt[1]['contents']['contents'][0]['hero']['url']);
+    return info_txt[i];
 }
 
 //產生min到max之間的亂數
