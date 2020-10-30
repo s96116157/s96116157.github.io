c4cabf900cbe364e4fe60470cd441f8eea47ab4a
s96116157 <s96116157@gmail.com>
2020-10-30 18:16:22 +0800

    up

--- mypage/LINE_Tarot.js
+++ mypage/LINE_Tarot.js
@@ -26,10 +26,12 @@ function doPost(e) {
         return;
     }
     var userType = msg.events[0].type;
-    getUserAnswer(clientID, '');
+    //getUserAnswer(clientID, '');
     switch (userType) {
         case 'follow':
-            sendReplyMessage(CHANNEL_ACCESS_TOKEN, replyToken, json_txt[0], json_txt[1]);
+            return_txt = getJson();
+            sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
+            return;
         case 'unfollow':
             return;
         case 'message':
@@ -54,15 +56,15 @@ function doPost(e) {
                     "previewImageUrl": "https://s96116157.github.io/image/A_00" + num + ".png"
                 };
 
-                response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/LINE_bubble.json');
                 //response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/info.json'); // get feed
+                response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/LINE_bubble.json');
                 var info_txt = JSON.parse(response.getContentText()); //
                 //var txt_2 = { "type": "text", "text": info_txt['info'][0]['txt'] };
                 //console.log('============== info_txt =================');
                 //console.log(info_txt);
 
                 //sendReplyMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt, txt_2)
-                sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, info_txt);
+                sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, info_txt[0]);
             }
             return;
     }
@@ -111,33 +113,41 @@ function sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, msg) {
 
 //判斷使用者回答到第幾題
 function getUserAnswer(clientID, clientMessage) {
-
-    var returnData = [];
-    for (var i = lastRow - 1; i >= 0; i--) {
-        if (sheetData[i][0] == clientID && sheetData[i][lastColumn - 1] == "") {
-            for (var j = 1; j <= lastColumn - 1; j++) {
-                if (sheetData[i][j] == "") { break; }
+    /*
+        var returnData = [];
+        for (var i = lastRow - 1; i >= 0; i--) {
+            if (sheetData[i][0] == clientID && sheetData[i][lastColumn - 1] == "") {
+                for (var j = 1; j <= lastColumn - 1; j++) {
+                    if (sheetData[i][j] == "") { break; }
+                }
+                sheet.getRange(i + 1, j + 1).setValue(clientMessage);
+                //如果使用者已經回答了最後一題，就把完成時間填上。不然就送出下一題給使用者
+                if (j + 2 == lastColumn) {
+                    sheet.getRange(i + 1, lastColumn).setValue(Date());
+                    returnData = [i + 1, 0];
+                }
+                else {
+                    returnData = [i + 1, j + 2];
+                }
+                return returnData;
+                break;
             }
-            sheet.getRange(i + 1, j + 1).setValue(clientMessage);
-            //如果使用者已經回答了最後一題，就把完成時間填上。不然就送出下一題給使用者
-            if (j + 2 == lastColumn) {
-                sheet.getRange(i + 1, lastColumn).setValue(Date());
-                returnData = [i + 1, 0];
-            }
-            else {
-                returnData = [i + 1, j + 2];
-            }
-            return returnData;
-            break;
         }
-    }
+    */
     //如果使用者還沒有回答過任何資料，就新增加一列在最後，把使用者ID輸入並開始送出題目
     sheet.insertRowAfter(lastRow);
     sheet.getRange(lastRow + 1, 1).setValue(clientID);
-    returnData = [lastRow + 1, 2];
-    console.log('=================== returnData ==================');
-    console.log(returnData);
-    return returnData;
+    sheet.getRange(lastRow + 1, 2).setValue(0);
+    //returnData = [lastRow + 1, 2];
+    //console.log('=================== returnData ==================');
+    //console.log(returnData);
+    //return returnData;
+}
+
+function getJson() {
+    var response = UrlFetchApp.fetch('https://s96116157.github.io/js/json/LINE_bubble.json');
+    var info_txt = JSON.parse(response.getContentText()); //
+    return info_txt[0];
 }
 
 //產生min到max之間的亂數
