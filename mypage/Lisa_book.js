aee970f17633dfcae1ad600c8c3226be4e48a56d
JASON <s96116157@gmail.com>
2020-11-19 12:00:39 +0800

    Lisa

--- mypage/Lisa_book.js
+++ mypage/Lisa_book.js
@@ -5,5 +5,105 @@ function doPost(e) {
     var userType = msg.events[0].type;
     var replyToken = msg.events[0].replyToken;
 
-    return;
+    if (typeof replyToken === 'undefined') { return; }
+
+    console.log('============ 202011181550 ============');
+    console.log(msg.events[0]);
+
+    switch (userType) {
+        case 'follow':
+            return_txt = get_follow_bubble();
+            sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
+            return;
+        case 'unfollow':
+            return;
+        case 'message':
+            return_txt = get_follow_bubble();
+            sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, return_txt);
+            return;
+        case 'postback':
+            return;
+        default:
+            return;
+
+    }
+}
+
+function sendPushMessage(CHANNEL_ACCESS_TOKEN, replyToken, msg) {
+    var url = 'https://api.line.me/v2/bot/message/reply';
+    UrlFetchApp.fetch(url, {
+        "headers": {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + CHANNEL_ACCESS_TOKEN,
+        },
+        "method": "post",
+        "payload": JSON.stringify({
+            "replyToken": replyToken,
+            "messages": [msg],
+        }),
+    });
+}
+
+function get_follow_bubble() {
+    var url = 'https://scontent.ftpe7-2.fna.fbcdn.net/v/t31.0-8/460837_281453208617694_470224812_o.jpg?_nc_cat=109&ccb=2&_nc_sid=de6eea&_nc_ohc=ajR8OvwytxIAX8tAg6b&_nc_ht=scontent.ftpe7-2.fna&oh=d3ab36cd41a7c2112157572bfe7c655f&oe=5FD94BE0';
+    var txt = {
+        "type": "flex",
+        "altText": "this is a flex message",
+        "contents": {
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": url,
+                "size": "full",
+                "aspectRatio": "20:15",
+                "aspectMode": "cover",
+                "backgroundColor": "#FFFFFFFF"
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "backgroundColor": "#535557FF",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "Hi. 我是 Clio",
+                        "weight": "bold",
+                        "size": "xl",
+                        "color": "#FFFFFFFF",
+                        "contents": []
+                    },
+                    {
+                        "type": "text",
+                        "text": "很開心我們成為了好友！\n自然塔羅是以牌卡占卜和大家分享：人生哲學、心靈成長、運勢解析\n沒有個人占卜、收費服務\n除了塔羅和閒聊以外，偶爾會天降趣味小測驗，單純想和大家分享塔羅的有趣，一起開心互動。",
+                        "size": "md",
+                        "color": "#FFFFFFFF",
+                        "wrap": true,
+                        "contents": []
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "flex": 0,
+                "spacing": "sm",
+                "backgroundColor": "#535557FF",
+                "contents": [
+                    {
+                        "type": "filler"
+                    },
+                    {
+                        "type": "text",
+                        "text": "輸入「塔羅」或點選下方\n「點我看更多功能」將會為您服務哦！",
+                        "size": "sm",
+                        "color": "#FFFFFFFF",
+                        "align": "center",
+                        "wrap": true,
+                        "contents": []
+                    }
+                ]
+            }
+        }
+    };
+    return txt;
 }
