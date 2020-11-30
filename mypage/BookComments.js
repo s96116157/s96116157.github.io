d7b4d97f8f6406e9c863666a6af685f5261f0f27
JASON <s96116157@gmail.com>
2020-11-30 17:24:48 +0800

    comm

--- mypage/BookComments.js
+++ mypage/BookComments.js
@@ -1,6 +1,11 @@
 var id = '1calxFtlDzNrK78vFKpX3AygxbB1VTrfMb10qK8wIe48';
 var url = 'https://spreadsheets.google.com/feeds/list/' + id + '/od6/public/values?alt=json';
 var _list = [];
+var user = [
+    'Uf9a505a47211f69d17cdf966789f9ca4',  // 瑄
+    'U4e05797dc82021cfa5dd34b1587e6b40', // Boots
+    'U578815852467d6824d56536024c75e03' // 貓咪
+]
 get_info();
 
 function get_info() {
@@ -10,11 +15,14 @@ function get_info() {
         for (i = len - 1; i >= 0; i--) {
             //var _info = 0 ? '' : data[i]['gsx$info']['$t'];
             _list.push({
+                user: data[i]['gsx$user']['$t'],
                 time: data[i]['gsx$time']['$t'],
-                info: data[i]['gsx$info']['$t']
+                info: data[i]['gsx$info']['$t'],
+                re: data[i]['gsx$return']['$t']
             });
         }
     });
+    console.log(_list);
 }
 
 var vm = new Vue({
