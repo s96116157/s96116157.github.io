4dc4923fff19d46dffddcd178ed1828618294750
JASON <s96116157@gmail.com>
2020-12-08 17:14:18 +0800

    id

--- mypage/BookComments.js
+++ mypage/BookComments.js
@@ -1,21 +1,25 @@
 var id = '1calxFtlDzNrK78vFKpX3AygxbB1VTrfMb10qK8wIe48';
 var url = 'https://spreadsheets.google.com/feeds/list/' + id + '/od6/public/values?alt=json';
 var _list = [];
-var L_id = '尚未登入 LINE';
+var LINE_id = '';
 
 async function main() {
     await liff.init({ liffId: "1655284249-Dl2J9P15" });
     if (liff.isLoggedIn()) {
         console.log(liff.getContext().userId);
-        L_id = liff.getContext().userId;
+        LINE_id = liff.getContext().userId;
+        alert("登入");
+        get_info();
     } else {
+        LINE_id = '尚未登入 LINE';
+        alert("尚未登入 LINE");
+        get_info();
         //liff.login();
     };
     //window.location.reload()
 }
 
 main();
-get_info();
 
 function get_info() {
     fetch(url).then(res => res.json()).then(lessons => {
@@ -39,10 +43,29 @@ var vm = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
     data: {
-        user_id: L_id,
+        user_id: LINE_id,
         list: _list,
         info_txt: ''
     },
     methods: {
+        send_info() {
+            var url = 'https://script.google.com/macros/s/AKfycbxyu-JHXikGYsUvRt4aMjv-UsPPjSr25tjX2X-qFcDtOCm8hN8/exec';
+            if (LINE_id != '尚未登入 LINE') {
+                $.ajax({
+                    url: url,
+                    data: {
+                        "info": this.info_txt,
+                        "id": this.user_id
+                    },
+                    success: function (response) {
+                        if (response == "ok") {
+                            window.location.reload();
+                        }
+                    },
+                });
+            } else {
+                alert("尚未登入 LINE");
+            }
+        }
     }
 });
